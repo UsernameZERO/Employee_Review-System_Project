91dfeb07af648d0cfbca26b01cd6b4385ad598f5
Zero <iamvamsi999@gmail.com>
2022-02-24 21:20:16 +0530

    .

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 //Connecting mongoose to mongodb
 // mongoose.connect(`mongodb://localhost/employee_review-systemPRz`);
-const dbUrl = 'mongodb+srv://UsernameZero:UsernameZero@cluster0.oe1eg.mongodb.net/employee_review-system_proz';
+const dbUrl = 'mongodb+srv://UsernameZero:UsernameZero@cluster0.oe1eg.mongodb.net/employee_review-system';
 mongoose.connect( dbUrl , {useNewUrlParser: true});
 
 const db = mongoose.connection;// storing the data from mongoDB to initialized variable
